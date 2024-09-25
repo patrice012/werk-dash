0d013a041e8b42f1d3df777d05aeaaa8c3e14936
SamuelAMZ <>
2024-09-25 22:25:20 +0000

    refetching data on detail page

--- src/components/DetailJob.tsx
+++ src/components/DetailJob.tsx
@@ -2,14 +2,15 @@
 import { apiGET } from "@/api/api";
 import { VITE_API_QUERY_LIMIT } from "@/helpers/constants";
 import Job from "@/models/job.model";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueries } from "@tanstack/react-query";
 import { Card } from "./Card";
-import { debounce } from "lodash";
 import { useRef, useState, useCallback, useEffect } from "react";
 import { SkeletonCard } from "./Skeleton";
 import { useLocation } from "react-router-dom";
 import { Separator } from "./ui/separator";
 import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import * as query from "react-query";
 
 export default function DetailJob() {
   const scrollRef = useRef<HTMLDivElement>(null);
@@ -17,29 +18,45 @@ export default function DetailJob() {
   const [currentPage] = useState(1);
   const navigate = useNavigate();
 
-  const { isPending, data, refetch } = useQuery({
+  const params = useParams();
+
+  console.log(params.id);
+
+  /*  const { isPending, data, refetch } = useQuery({
     queryKey: ["repoDjata"],
     queryFn: async () =>
       await apiGET({
         uri: `/jobs/search/?searchValue=${displayTerm}&page=${currentPage}&limit=${VITE_API_QUERY_LIMIT}`,
       }),
-  });
+  }); */
 
-  const {} = useQuery({
-    queryKey: ["JobDetail"],
-    queryFn: async () =>
-      await apiGET({
-        uri: `/jobs/`,
-      }),
+  const [AllQuery, DetailQuery] = useQueries({
+    queries: [
+      {
+        queryKey: ["repoDjata"],
+        queryFn: async () =>
+          await apiGET({
+            uri: `/jobs/search/?searchValue=${displayTerm}&page=${currentPage}&limit=${VITE_API_QUERY_LIMIT}`,
+          }),
+      },
+
+      {
+        queryKey: ["Detail"],
+        queryFn: async () =>
+          await apiGET({
+            uri: `/jobs/${params.id}`,
+          }),
+      },
+    ],
   });
 
   useEffect(() => {
-    refetch();
-  }, [displayTerm, refetch]);
+    AllQuery.refetch();
+  }, [displayTerm, AllQuery.refetch]);
 
   const handleSelectItem = (item: Job) => {
     window.scrollTo({ top: 0, behavior: "smooth" });
-    navigate(`/job/${item._id}`, { state: { selectedItem: item } });
+    navigate(`/job/${item._id} `, { state: { selectedItem: item } });
   };
 
   const location = useLocation();
@@ -47,12 +64,15 @@ export default function DetailJob() {
   const [selectedJob, setSelectedJob] = useState<Job | null>(null);
 
   useEffect(() => {
-    if (location.state?.selectedItem) {
-      setSelectedJob(location.state.selectedItem);
+    if (params.id) {
+      DetailQuery.refetch();
+      if (location?.state?.selectedItem) {
+        setSelectedJob(location?.state.selectedItem);
+      }
       window.scrollTo({ top: 0 });
       console.log("first maison");
     }
-  }, [location.state]);
+  }, [location.state, params]);
 
   return (
     <div className="bg-[#f0f5fa] p-[15px] md:p-[30px] ">
@@ -62,12 +82,12 @@ export default function DetailJob() {
           className={`grid grid-rows-1 gap-[20px]  scrollbar scrollbar-thumb-[#d4d4d4]  scrollbar-w-[7px] scrollbar-thumb-rounded-full ${
             selectedJob != undefined && "hidden lg:grid"
           }`}>
-          {isPending &&
+          {AllQuery.isPending &&
             Array.from({ length: 9 }).map((_e, idx: number) => {
               return <SkeletonCard key={idx} />;
             })}
 
-          {data && data.data?.length > 0 && (
+          {AllQuery.data && AllQuery.data.data?.length > 0 && (
             <>
               {/* Affichez l'élément sélectionné en premier s'il existe */}
               {selectedJob && (
@@ -78,7 +98,7 @@ export default function DetailJob() {
                   onPress={() => handleSelectItem(selectedJob)}
                 />
               )}
-              {data.data
+              {AllQuery.data.data
                 .filter((item: Job) => item.jobTitle !== selectedJob?.jobTitle) // Filtrer l'élément sélectionné
                 .map((item: Job, idx: number) => (
                   <Card
@@ -94,85 +114,94 @@ export default function DetailJob() {
 
         {/* Div détails - occupe toute la largeur sous 1024px */}
         <div
-          className={`${
-            selectedJob?.jobTitle ? "sticky top-[20px]" : "hidden"
-          }  top-[20px] col-span-3 lg:col-span-2 lg:max-h-max bg-[#fff] rounded-[16px] p-[15px] lg:px-[48px] lg:py-[32px] w-full max-w-full`}>
-          <div className="md:grid md:grid-cols-7 md:col-span-7">
-            <div className="md:grid col-span-5 md:border-r md:pr-[15px] border-[#d4d4d4]">
-              <div className="sm:grid flex flex-col w-full col-span-6 grid-cols-6 justify-between gap-[12px] items-start  sm:items-center mb-[24px]">
-                <div className="overflow-hidden col-span-5">
-                  <h1 className="font-[800] text-[#303533] text-20-title truncate">
-                    {selectedJob?.jobTitle}
-                  </h1>
-                </div>
+          className={`${"sticky top-[20px]"}  top-[20px] col-span-3 lg:col-span-2 lg:max-h-max bg-[#fff] rounded-[16px] p-[15px] lg:px-[48px] lg:py-[32px] w-full max-w-full`}>
+          {DetailQuery.isFetching && <SkeletonCard />}
 
-                <div className="flex gap-[8px] w-[200px] col-span-1  ">
-                  <div className="bg-[#f1e3ff] px-[5px] py-[3px] text-[12px]  text-[#7744aa] font-semibold rounded-[4px]">
-                    {selectedJob?.employmentType}
+          {!DetailQuery.isRefetching && (
+            <div className="md:grid md:grid-cols-7 md:col-span-7">
+              <div className="md:grid col-span-5 md:border-r md:pr-[15px] border-[#d4d4d4]">
+                <div className="sm:grid flex flex-col w-full col-span-6 grid-cols-6 justify-between gap-[12px] items-start  sm:items-center mb-[24px]">
+                  <div className="overflow-hidden col-span-5">
+                    <h1 className="font-[800] text-[#303533] text-20-title truncate">
+                      {DetailQuery.data?.job[0]?.jobTitle}
+                    </h1>
+                  </div>
+
+                  <div className="flex gap-[8px] w-[200px] col-span-1  ">
+                    <div className="bg-[#f1e3ff] px-[5px] py-[3px] text-[12px]  text-[#7744aa] font-semibold rounded-[4px]">
+                      {DetailQuery.data?.job[0]?.employmentType}
+                    </div>
                   </div>
                 </div>
+                <div className="flex w-full md:hidden ">
+                  <div className="flex flex-col w-full gap-[6px]">
+                    <span className="font-[600]">
+                      {DetailQuery.data?.job[0]?.companyName}
+                    </span>
+                    <div className="flex md:flex-col flex-row gap-[6px]">
+                      <span className="text-[12px] font-[600]">Location:</span>
+                      <span className="truncate text-wrap overflow-hidden font-[500] text-[12px] ">
+                        {DetailQuery.data?.job[0]?.location}
+                      </span>
+                    </div>
+                    {DetailQuery.data?.job[0]?.companyWebsite && (
+                      <div className="flex flex-col gap-[6px]">
+                        <span className="text-[12px] font-[600]">
+                          Site web:
+                        </span>
+                        <span className="truncate text-ellipsis font-[500] text-[12px]">
+                          {DetailQuery.data?.job[0]?.companyWebsite}
+                        </span>
+                      </div>
+                    )}
+
+                    <a
+                      href={DetailQuery.data?.job[0]?.jobUrl}
+                      target="_blank"
+                      className="bg-[#207fff] px-[15px] rounded-full  w-full py-[12px] transition ease-in-out duration-500 text-center hover:bg-[#2081ffd4] text-[#fff]">
+                      Apply now
+                    </a>
+                  </div>
+                </div>
+                <Separator className="my-[15px] bg-[#d4d4d4]" />
+                <div
+                  className="content-intro"
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      DetailQuery.data?.job[0]?.jobDescriptionRawHtml || "",
+                  }}
+                />
               </div>
-              <div className="flex w-full md:hidden ">
-                <div className="flex flex-col w-full gap-[6px]">
-                  <span className="font-[600]">{selectedJob?.companyName}</span>
-                  <div className="flex md:flex-col flex-row gap-[6px]">
+              <div className="hidden md:grid col-span-2 md:pl-[15px]">
+                <div className="flex flex-col gap-[24px]">
+                  <span className="font-[600]">
+                    {DetailQuery.data?.job[0]?.companyName}
+                  </span>
+                  <div className="flex flex-col gap-[6px]">
                     <span className="text-[12px] font-[600]">Location:</span>
                     <span className="truncate text-wrap overflow-hidden font-[500] text-[12px] ">
-                      {selectedJob?.location}
+                      {DetailQuery.data?.job[0]?.location}
                     </span>
                   </div>
-                  {selectedJob?.companyWebsite && (
+                  {DetailQuery.data?.job[0]?.companyWebsite && (
                     <div className="flex flex-col gap-[6px]">
                       <span className="text-[12px] font-[600]">Site web:</span>
                       <span className="truncate text-ellipsis font-[500] text-[12px]">
-                        {selectedJob?.companyWebsite}
+                        {DetailQuery.data?.job[0]?.companyWebsite}
                       </span>
                     </div>
                   )}
 
                   <a
-                    href={selectedJob?.jobUrl}
+                    href={DetailQuery.data?.job[0]?.jobUrl}
                     target="_blank"
-                    className="bg-[#207fff] px-[15px] rounded-full  w-full py-[12px] transition ease-in-out duration-500 text-center hover:bg-[#2081ffd4] text-[#fff]">
+                    className="bg-[#207fff] px-[15px] rounded-full  py-[12px] transition ease-in-out duration-500 text-center hover:bg-[#2081ffd4] text-[#fff]">
                     Apply now
                   </a>
                 </div>
               </div>
-              <Separator className="my-[15px] bg-[#d4d4d4]" />
-              <div
-                className="content-intro"
-                dangerouslySetInnerHTML={{
-                  __html: selectedJob?.jobDescriptionRawHtml || "",
-                }}
-              />
             </div>
-            <div className="hidden md:grid col-span-2 md:pl-[15px]">
-              <div className="flex flex-col gap-[24px]">
-                <span className="font-[600]">{selectedJob?.companyName}</span>
-                <div className="flex flex-col gap-[6px]">
-                  <span className="text-[12px] font-[600]">Location:</span>
-                  <span className="truncate text-wrap overflow-hidden font-[500] text-[12px] ">
-                    {selectedJob?.location}
-                  </span>
-                </div>
-                {selectedJob?.companyWebsite && (
-                  <div className="flex flex-col gap-[6px]">
-                    <span className="text-[12px] font-[600]">Site web:</span>
-                    <span className="truncate text-ellipsis font-[500] text-[12px]">
-                      {selectedJob?.companyWebsite}
-                    </span>
-                  </div>
-                )}
-
-                <a
-                  href={selectedJob?.jobUrl}
-                  target="_blank"
-                  className="bg-[#207fff] px-[15px] rounded-full  py-[12px] transition ease-in-out duration-500 text-center hover:bg-[#2081ffd4] text-[#fff]">
-                  Apply now
-                </a>
-              </div>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -6,8 +6,8 @@ import Job from "@/models/job.model";
 import { apiGET } from "@/api/api";
 import { SkeletonCard } from "./Skeleton";
 import { debounce } from "lodash";
-import { VITE_API_QUERY_LIMIT } from "@/helpers/constants";
 import { Card } from "./Card";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Page() {
   const scrollRef = useRef<HTMLDivElement>(null);
@@ -16,27 +16,40 @@ export default function Page() {
   const [displayTerm, setDisplayTerm] = useState("");
   const [currentPage] = useState(1);
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [NumberPage, setPage] = useState(12);
 
   //
   const { isPending, data, refetch } = useQuery({
     queryKey: ["repoDjata"],
     queryFn: async () =>
       await apiGET({
-        uri: `/jobs/search/?searchValue=${displayTerm}&page=${currentPage}&limit=${VITE_API_QUERY_LIMIT}`,
+        uri: `/jobs/search/?searchValue=${displayTerm}&page=${currentPage}&limit=${NumberPage}`,
       }),
-    // queryFn: async () => await apiGET({ uri: "/jobs" }),
   });
+
+  const fetchData = async () => {
+    try {
+      setPage((prevPage: number) => prevPage + 12);
+      console.log("text");
+    } catch (error) {
+      setError("");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   const handleSearch = useCallback(
     debounce((term) => {
       setDisplayTerm(term);
-    }, 500), // Debounced to execute 500ms after the user stops typing
+    }, 500),
     []
   );
 
   useEffect(() => {
     refetch();
-  }, [displayTerm, refetch]);
+  }, [displayTerm, refetch, NumberPage]);
 
   //
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -55,28 +68,42 @@ export default function Page() {
   return (
     <div className="flex flex-col w-full gap-[30px] px-[15px] md:px-[20px] pb-[30px] scrollbar scrollbar-thumb-[#d4d4d4]  scrollbar-w-[7px] scrollbar-thumb-rounded-full">
       {/*  */}
-      <div ref={scrollRef} className="grid grid-cols-3 gap-5 items-start">
-        {/* Première div - affichée uniquement au-dessus de 1024px */}
-        <div
-          className={`grid ${"grid-cols-1 md:grid-cols-2 lg:grid-cols-3 col-span-3"} gap-[20px] `}>
-          {isPending &&
-            Array.from({ length: 9 }).map((_e, idx: number) => {
+      <InfiniteScroll
+        dataLength={NumberPage}
+        next={fetchData}
+        hasMore={true} // Replace with a condition based on your data source
+        loader={
+          <div
+            className={`grid ${"grid-cols-1 md:grid-cols-2 lg:grid-cols-3 col-span-3"} gap-[20px] mt-[24px]`}>
+            {Array.from({ length: 3 }).map((_e, idx: number) => {
               return <SkeletonCard key={idx} />;
             })}
-          {data &&
-            data.data?.map((item: Job, idx: number) => {
-              const selected = selectedItem == idx;
-              return (
-                <Card
-                  job={item}
-                  isSelected={selected}
-                  key={idx}
-                  onPress={() => handleSelectItem(item, idx)}
-                />
-              );
-            })}
+          </div>
+        }
+        endMessage={<p>No more data to load.</p>}>
+        <div ref={scrollRef} className="grid grid-cols-3 gap-5 items-start">
+          {/* Première div - affichée uniquement au-dessus de 1024px */}
+          <div
+            className={`grid ${"grid-cols-1 md:grid-cols-2 lg:grid-cols-3 col-span-3"} gap-[20px] `}>
+            {isPending &&
+              Array.from({ length: 9 }).map((_e, idx: number) => {
+                return <SkeletonCard key={idx} />;
+              })}
+            {data &&
+              data.data?.map((item: Job, idx: number) => {
+                const selected = selectedItem == idx;
+                return (
+                  <Card
+                    job={item}
+                    isSelected={selected}
+                    key={idx}
+                    onPress={() => handleSelectItem(item, idx)}
+                  />
+                );
+              })}
+          </div>
         </div>
-      </div>
+      </InfiniteScroll>
     </div>
   );
 }
